67dfb86dc97be04ba9d6a80c0ad99fb3d88a7a77
Andrés Carrillo Herrera <andresjch2804@gmail.com>
2022-07-12 11:53:48 -0500

    e2e test for individual pokemon

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,16 @@ describe("Pokedex", function () {
     cy.contains("ivysaur")
     cy.contains("Pokémon and Pokémon character names are trademarks of Nintendo.")
   })
+
+  it("specific pages can be opened", function () {
+    cy.visit("http://localhost:5000")
+    cy.get("a[href*=\"/pokemon/venusaur\"]").click()
+    cy.contains("overgrow")
+    cy.contains("chlorophyll")
+
+    cy.visit("http://localhost:5000")
+    cy.get("a[href*=\"/pokemon/charmander\"]").click()
+    cy.contains("blaze")
+    cy.contains("solar power")
+  })
 })
