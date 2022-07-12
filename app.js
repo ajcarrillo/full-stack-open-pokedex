2388be603862548924b1df9b4f42718878688c76
Andrés Carrillo Herrera <andresjch2804@gmail.com>
2022-07-12 14:47:59 -0500

    health check

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express")
 const app = express()
 
+app.get("/health", (req, res) => {
+  res.send("ok")
+})
+
+app.get("/version", (req, res) => {
+  res.send("1") // change this string to ensure a new version deployed
+})
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
 app.use(express.static("dist"))
 
 app.listen(PORT, () => {
-  console.log("server started on port 5000")
+  console.log(`Listening on port ${PORT}`)
 })
